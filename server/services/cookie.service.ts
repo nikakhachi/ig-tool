62169d5c7166faa3de623eb58249570d44f2104a
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-07-01 15:14:50 +0400

    TEMPORAL! set cookie attribute secure to false

--- server/services/cookie.service.ts
+++ server/services/cookie.service.ts
@@ -1,11 +1,13 @@
 import { Response } from "express";
 import logger from "../utils/logger";
 
+logger.warn("Cookie secure attribute is set to False. Once we have HTTPS, we need to change it immediately!");
+
 const setAccessTokenCookie = (res: Response, accessToken: string) => {
   logger.debug("Setting Access Token Cookie");
   res.cookie("accessToken", accessToken, {
     sameSite: "none",
-    secure: true,
+    secure: false,
     httpOnly: true,
   });
 };
@@ -14,7 +16,7 @@ const setRefreshTokenCookie = (res: Response, refreshToken: string) => {
   logger.debug("Setting Refresh Token Cookie");
   res.cookie("refreshToken", refreshToken, {
     sameSite: "none",
-    secure: true,
+    secure: false,
     httpOnly: true,
   });
 };
