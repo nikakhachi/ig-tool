1053ebd0070b2f089bf0642a677a0b229867e1a1
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-07-02 14:21:46 +0400

    add demo visualization of data

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -3,6 +3,11 @@ import { useContext } from 'react';
 import styles from './styles.module.css';
 import { UserContext } from '../../contexts/UserContext';
 import { useConnectionsByDaysProvider } from '../../hooks/useConnectionsByDaysProvider';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function Home() {
   const userContext = useContext(UserContext);
@@ -15,7 +20,32 @@ function Home() {
   ) : (
     <div className={styles.container}>
       <h2>{user?.username}</h2>
-      <pre>{JSON.stringify(connectionsByDaysProvider.data, null, 2)}</pre>
+      {Object.keys(connectionsByDaysProvider.data).map((date) => (
+        <Accordion sx={{ width: 750 }}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+            <Typography>{date}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {Object.keys(connectionsByDaysProvider.data[date]).map((connectionType) => {
+              // eslint-disable-next-line
+              // @ts-ignore
+              const connections = connectionsByDaysProvider.data[date][connectionType];
+              return (
+                <Accordion>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+                    <Typography>
+                      {connectionType} {connections.length}
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails sx={{ height: 200, overflowY: 'scroll' }}>
+                    <pre>{JSON.stringify(connections, null, 2)}</pre>
+                  </AccordionDetails>
+                </Accordion>
+              );
+            })}
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </div>
   );
 }
